4ba4df538f5ae4e6258a79dce6599902489d4886
Sangw0n <pilwon070127@naver.com>
2024-01-09 23:40:00 +0900

    Code Annotation Add

--- Assets/1. Script/0. Player/Player.cs
+++ Assets/1. Script/0. Player/Player.cs
@@ -32,5 +32,14 @@ public class Player : MonoBehaviour
         anim.SetBool("isWalk", isWalkDown);
 
         transform.LookAt(transform.position + moveVec);
+        /*
+            다음에 이동할 방향 ( transform.position + moveVec )
+            transform.position : 자기위치 기준
+            내 위치에서 다음 방향 값을 더해 내가 이동할 위치를 구함
+            
+            Ex ) (0, 0, 0) + (0, 0, 1) -> 오른쪽으로 이동
+                 오른쪽으로 회전 
+        */
+
     }
 }
